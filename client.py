7c707fba9297d69ea3f6d0e08a568b9013e4ac28
Vijendra-Achar <vijendrachar.m@gmail.com>
2023-11-08 23:21:18 +0000

    Camera image is being captured

--- client.py
+++ client.py
@@ -1,12 +1,17 @@
 # importing everything we need
 import cv2
 import csv
+import time
 
-# select the video source; 0 - integrated webcam; 1 - external webcam;
+# Select the Video source
 VIDEO_SOURCE = 1
 
 # start the recording
 cap = cv2.VideoCapture(VIDEO_SOURCE)
+
+# Allow the camera to start (To avoid the blank first frame)
+time.sleep(3)
+
 ret, image = cap.read()
 
 # If the return value form the VideoCapture is true, then write the image file
@@ -15,18 +20,21 @@ if ret:
     cv2.imwrite("data/frame0.jpg", image)
     img = cv2.imread("data/frame0.jpg")
 
-    # get the regions of interest/parking spots
+    # Get the regions of interest/parking spots
     r = cv2.selectROIs('ROI Selector', img, showCrosshair=False, fromCenter=False)
 
-    # convert the result to a list
-    rlist = r.tolist()
-    print(rlist)
+    if len(r):
+        # Convert the result to a list
+        rlist = r.tolist()
+        print(rlist)
 
-    # write the list into a csv file
-    with open('data/rois.csv', 'w', newline='') as outf:
-        csvw = csv.writer(outf)
-        csvw.writerows(rlist)
+        # Write the list into a csv file
+        with open('data/rois.csv', 'w', newline='') as outf:
+            csvw = csv.writer(outf)
+            csvw.writerows(rlist)
+    else:
+        print("Please select the Region of interest")
 
-# when everything is done, release the capture
+# When everything is done, release the capture
 cap.release()
 cv2.destroyAllWindows()
